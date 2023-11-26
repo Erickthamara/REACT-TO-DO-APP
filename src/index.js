316eac5230f998283c25d9c4d8f5a424c4d9ba7f
Erickthamara <105923301+Erickthamara@users.noreply.github.com>
2023-11-26 07:47:30 +0300

    Simple to do app using React
    Learned that i need to learn use state inorder to build the app better

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const ToDoList = () => {
+  const AddedTask = document.getElementById("addedTask");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const task = () => console.log(AddedTask.value);
+  return (
+    <div>
+      <h1>TODO-LIST</h1>
+
+      <input
+        type="text"
+        className="TaskAdedd"
+        id="addedTask"
+        placeholder="Input Your task here"
+      />
+      <button onClick={task}>SUBMIT</button>
+
+      <ul>
+        <li>
+          <button>X</button>Task 1<button>DONE</button>
+        </li>
+        <li>
+          <button>X</button>Task 2<button>DONE</button>
+        </li>
+        <li>
+          <button>X</button>Task 3<button>DONE</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<ToDoList />);
